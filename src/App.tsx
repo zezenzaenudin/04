5ba29a1de1f62dc089d65f07e804be4187103076
zezen zaenudin <zezenzaenudin@gmail.com>
2024-09-30 10:42:52 +0700

    feat:init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="p-[10em]CD.. border-0 bg-amber-200">
+        <h2>ini tailwind</h2> <br />
+        <p className="bg-red-200 caret-red-500 fill-orange-500">test css</p>
+    </div>
+  )
+}
+
+export default App
